35c353230159c31b549009c7caf4f59e9a06c592
Sergey <supermatrix2001@gmail.com>
2026-07-17 01:16:51 +0300

    backend(fix): add trust proxy to fix express-rate-limit behind Nginx

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,6 +10,8 @@ import { errorHandler } from './middlewares/errorHandler';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('trust proxy', 1);
+
 // Middleware
 app.use(
 	cors({
